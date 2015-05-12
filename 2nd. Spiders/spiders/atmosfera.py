5b1795727322d9a53bc4481cbcaba7f4f16fb09f
Leonel Vinicio Morales Díaz <litomd@gmail.com>
2015-05-12 12:38:59 -0600

    Update atmosfera.py
    Dos cambios: usar replace en la asignación a item['cancion'] para eliminar los números de canción y usar un if antes del yield para evitar que cree líneas en blanco.
    
    Favor de probar

--- 2nd. Spiders/spiders/atmosfera.py
+++ 2nd. Spiders/spiders/atmosfera.py
@@ -14,8 +14,9 @@ class AtmosferaSpider(scrapy.Spider):
         
          for sel in response.xpath('//div/div/div/div/div/ul/li/div/div'):
             item = CancionItem() 
-            item['cancion'] = sel.xpath('p[contains(@class, "top-song")]/text()').extract();
+            item['cancion'] = sel.xpath('p[contains(@class, "top-song")]/text()').extract().replace('^\d+\. ','');
             item['artista'] = sel.xpath('p[contains(@class, "top-autor")]/text()').extract();
             item['disco'] = sel.xpath('p[contains(@class, "top-disco")]/text()').extract();
             
-            yield item 
+            if item['cancion'] != '': 
+                yield item 
